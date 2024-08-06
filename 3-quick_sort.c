6d359469205019465316898013acb921f4eeeb58
HSHKZ <lucasolegniel@gmail.com>
2024-08-06 17:00:56 +0200

    function that sorts an array of integers in ascending order using the Selection sort algorithm

---
+++ 3-quick_sort.c
@@ -0,0 +1,71 @@
+#include "sort.h"
+
+/**
+ * quick_sort - Sorts an array of integers in
+ * ascending order using Quick sort
+ * @array: Pointer to the array to be sorted
+ * @size: Size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+	quick_sort_recursive(array, 0, size - 1, size);
+}
+
+/**
+ * quick_sort_recursive - Recursive helper function
+ * for Quick sort
+ * @array: Pointer to the array to be sorted
+ * @low: Starting index of the partition
+ * @high: Ending index of the partition
+ * @size: Size of the array
+ */
+void quick_sort_recursive(int *array, int low, int high, size_t size)
+{
+	int pivot;
+
+	if (low < high)
+	{
+		pivot = lomuto_partition(array, low, high, size);
+		quick_sort_recursive(array, low, pivot - 1, size);
+		quick_sort_recursive(array, pivot + 1, high, size);
+	}
+}
+
+/**
+ * lomuto_partition - Lomuto partition scheme
+ * for Quick sort
+ * @array: Pointer to the array to be sorted
+ * @low: Starting index of the partition
+ * @high: Ending index of the partition
+ * @size: Size of the array
+ * Return: Index of the pivot after partitioning
+ */
+
+int lomuto_partition(int *array, int low, int high, size_t size)
+{
+	int i = low, j, pivot = array[high], temp;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+
+			if (temp != array[i])
+				print_array(array, size);
+
+			++i;
+		}
+	}
+
+	temp = array[i];
+	array[i] = array[high];
+	array[high] = temp;
+
+	if (temp != array[i])
+		print_array(array, size);
+
+	return (i);
+}
