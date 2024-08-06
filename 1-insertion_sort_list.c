10a4b6ff1d7967f832924ffa805cc6bd5b7014d3
HitteAnzo <hitteanzo@gmail.com>
2024-08-06 10:07:55 +0200

    Creation and fix betty to 1-

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a double linked list with insertion sort
+ * @list: the list of integers to sort
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *temp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	for (current = (*list)->next; current != NULL; current = current->next)
+	{
+		temp = current;
+		while (temp->prev != NULL && temp->n < temp->prev->n)
+		{
+			temp->prev->next = temp->next;
+			if (temp->next != NULL)
+				temp->next->prev = temp->prev;
+
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+
+			if (temp->prev != NULL)
+				temp->prev->next = temp;
+			else
+				*list = temp;
+
+			temp->next->prev = temp;
+			print_list(*list);
+		}
+	}
+}
