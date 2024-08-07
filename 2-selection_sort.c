175fbeebfa8e38b58fd07ed5da37a5ee3bd1f8a7
HitteAnzo <hitteanzo@gmail.com>
2024-08-07 17:50:48 +0200

    fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -4,7 +4,6 @@
  * selection_sort - sorts an array of int in ascending order
  * @array: array of int to sort
  * @size: size of the array
- *
  * Return: Exit the function if array is NULL or size less than 2
  */
 
